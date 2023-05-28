6d325560e296aa0c43afd618bd6514e99bfdddcd
mudassir060 <mudassirmukhtar4@gmail.com>
2023-05-28 19:36:19 +0500

    provider

---
+++ lib/Provider/providerScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class providerScreen extends StatefulWidget {
+  const providerScreen({super.key});
+
+  @override
+  State<providerScreen> createState() => _providerScreenState();
+}
+
+class _providerScreenState extends State<providerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
